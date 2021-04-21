94f3604835703d5e2b4a930f6dc692bd95c53228
Alexey Serov (DevExpress) <Serov.Alexey@corp.devexpress.com>
2021-04-21 22:50:42 +0300

    working on reports

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ import widgets from './widgetList.js';
 import through from 'through2';
 import webpack from 'webpack-stream';
 import rename from 'gulp-rename';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 let __dirname = path.resolve()
 
@@ -12,22 +13,32 @@ function rebase(content, name){
         var transformedFile = vinylFile.clone();    
         transformedFile.contents = Buffer.from(content);
         transformedFile.cwd = '/',
-        transformedFile.base = '/indices/',
-        transformedFile.path = `/indices/${name}.js`,
+        transformedFile.base = 'generated/indices/',
+        transformedFile.path = `generated/indices/${name}.js`,
         callback(null, transformedFile);
       });        
 }
 
 function processWidget(widget){
     return done => {
-        gulp.src('./src/stub.js')
+        gulp.src('./stubs/stub.js')
         .pipe(rebase(`import STUB_NAME from '${widgets.all[widget]}'`, widget))                
-        .pipe(gulp.dest('indices')) 
-        .pipe(webpack())
+        .pipe(gulp.dest('generated/indices')) 
+        .pipe(webpack({
+            plugins: [
+                new BundleAnalyzerPlugin({
+                    analyzerMode: 'static',
+                    reportFilename: `generated/reports/${widget}/analyzer.html`,
+                    openAnalyzer: false,
+                    generateStatsFile: true,
+                    statsFilename: `generated/reports/${widget}/data.json`,                    
+                })
+            ]
+        }))
         .pipe(rename(x=>{
             x.basename = widget;
         }))
-        .pipe(gulp.dest('bundles'))        
+        .pipe(gulp.dest('generated/bundles'))        
         .on('end', ()=>done());
     }
 }
@@ -36,6 +47,7 @@ function processWidgets(){
     let result = [];
     for(let widget in widgets.all){
         result.push(processWidget(widget));
+        break;
     }
     return result;
 }
